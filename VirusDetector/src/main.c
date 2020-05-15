4196040d5c2ea8f949313c6efab38255c35eb808
yoav <yoav.shalev67@gmail.com>
2020-05-15 20:24:26 +0300

    update file

--- VirusDetector/src/main.c
+++ VirusDetector/src/main.c
@@ -6,40 +6,41 @@ file: main.c
 
 #include "../include/viruslist.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
-	FILE* infected = NULL;
-	FILE* signatures = NULL;
 	int infectedFileSize;
 	char* virusBuffer;
 	link* viruslist = NULL;
-
-	/*if (argc == 1) {
-		printf("Error: there is no files to process!!\n");
-		return EXIT_FAILURE;
-	}*/
+	FILE* infected = NULL;
+	FILE* signatures = NULL;
 
 	infected = fopen(argv[1], "r+b");
 	signatures = fopen(argv[2], "r+b");
 
-	viruslist = make_list(signatures); /* create the list of signatures*/
+	if(infected == NULL || signatures == NULL) {
+		fprintf(stderr, "Can't open your files\n");
+		return EXIT_FAILURE;
+	}
 
-	/*list_print(viruslist);*/
+	viruslist = make_list(signatures); /* create the list of signatures*/
 
 	/* allocate and init with zero */
 	virusBuffer = (char*)calloc(sizeof(char), 10000);
 
 	/* store the infected file in buffer */
-	infectedFileSize = fread(&virusBuffer, sizeof(char), 10000, infected);
-	printf("%d\n", infectedFileSize);
+	infectedFileSize = fread(virusBuffer, sizeof(char), 10000, infected);
 
-	detect_virus(virusBuffer , viruslist, infectedFileSize); /* assume that the file is smaller than the buffer */
+	detect_virus(virusBuffer , viruslist, infectedFileSize); /*assume that the file is smaller than the buffer */
+
+	/*fclose(infected);
+	fclose(signatures);*/
+
+	list_print(viruslist);
 
 	list_free(viruslist);
 
-	fclose(infected);
-	fclose(signatures);
-
+	free(virusBuffer);
+	
 	return EXIT_SUCCESS;
 }
 
